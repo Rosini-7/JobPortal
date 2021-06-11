d935749214f34af5da7795684c34a023fe8345e2
Rosini-7 <79824012+Rosini-7@users.noreply.github.com>
2021-06-11 11:36:43 +0530

    first commit

---
+++ src/app/user.ts
@@ -0,0 +1,13 @@
+
+export class User {
+    id:number|undefined;
+    emailId:string|undefined;
+    userName:string|undefined;
+    password:string|undefined;
+    constructor(){
+            
+    }
+}
+        
+    
+
